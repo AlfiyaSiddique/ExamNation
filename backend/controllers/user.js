4d414f1c472049c08683f40030798273e9cce71e
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2025-03-27 15:56:27 +0530

    Error fix

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
+    console.log(req.body)
     try{
     let user = await  User.findOne({email: req.body.email});
     if(user) return res.status(400).json({success: false, message: "User already exist"})
